b4e3d56a4eee8dcda95ef2739f359678599b2ce1
kvdep <jack_ggg@mail.ru>
2025-02-23 18:04:22 +0300

    Edited client.py and added eternal while True loop

--- 1lab/client.py
+++ 1lab/client.py
@@ -1,25 +1,63 @@
 import socket
 
-def send_request_to_server(message, host='127.0.0.1', port=57394):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((host, port))
-            print(f"Connected a server with this adress: {host}:{port}")
-            client_socket.sendall(message.encode('utf-8'))
-            print(f"Message sent to {host}:{port}")
-            client_socket.shutdown(socket.SHUT_WR)
 
-    
-            data = client_socket.recv(1024)
-            print(f"Message recieved: {data.decode('utf-8')}")
+def send_request_to_server(message, host="127.0.0.1", port=57394):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((host, port))
+        print(f"Connected a server with this adress: {host}:{port}")
+        client_socket.sendall(message.encode("utf-8"))
+        print(f"Message sent to {host}:{port}")
+        client_socket.shutdown(socket.SHUT_WR)
+
+        #data = client_socket.recv(1024)
+        data = ""
+        part = client_socket.recv(1024).decode("utf-8")
+        print("Recieving package")
+        while part:
+            data += part
+            part = client_socket.recv(1024).decode("utf-8")
+            #print(data)
+        #print(f"Packet recieved: {data}")
+        print(f"Packet recieved.")
+
+        #обработаем запрос 
+        status = data[:data.find('.')]
+        body = data[data.find('.')+1:]
+        return (status,body)
+        #print(f"Message recieved: {status}")
+
+def start_client(host="127.0.0.1", port=57394):
+    print('Client initiated, standby.\n')
+    print(f'Available commands:\n')
+    print(f'JSON_IT <=> вывод json файла с названиям таблиц и их столбцами\nSELECT/select ... from ... where(optional) ... <=> select-запрос\nНазвание|Файл <=> запись csv файла')
+    while True:
+        message = input("\nEnter your request (or 'shutdown' to quit):\n")
+        if message.strip().lower()[:8] == 'shutdown':
+            print("Exiting client.")
+            break
+        while (message.strip()[-1]!=';'):
+            message+=input()
         
 
+        print('\nEstablishing connection to server.\n')
+        status,body = send_request_to_server(message,host,port)
+        print(f'Inquiry status: {status}')
+        if status=='Success':
+            print(f'------------------------\n')
+            print(body)
+            print(f'\n------------------------\n')
+
+
+
+
+        
 
 message = """
 select name as name
 from cities as c
 where name != city
 """
-message="""city1|name,age,city
+message = """city1|name,age,city
 Alice,30,New York
 Bob,25,Los Angeles
 Charlie,35,Chicago"""
@@ -29,10 +67,11 @@ from city1 as c
 where name != city
 """
 
-#s = JSON_IT <=> JSON_IT - вывод json файла с названиям таблиц и их столбцами
-#s = SELECT/select ... from ... where(optional) ...<=> select-запрос
-#s = Название|Файл <=> запись csv файла
+# s = JSON_IT <=> JSON_IT - вывод json файла с названиям таблиц и их столбцами
+# s = SELECT/select ... from ... where(optional) ...<=> select-запрос
+# s = Название|Файл <=> запись csv файла
 
 
-print(message)
-send_request_to_server(message)
+#print(message)
+#send_request_to_server(message)
+start_client()
--- 1lab/server.py
+++ 1lab/server.py
@@ -2,21 +2,27 @@ import socket
 from multiprocessing import Process
 from server_functions import client_handler
 from server_functions import init
+
 init()
 
-def client_handler_server(data,sock):
+
+def client_handler_server(data, sock):
     result = client_handler(data)
-    sock.sendall(result[0].encode('utf-8'))
-    sock.sendall(result[1].encode('utf-8'))
-    print('Results sent! Closing socket')
+    sock.sendall(result[0].encode("utf-8"))
+    sock.sendall(result[1].encode("utf-8"))
+    #sock.sendall(result[0])
+    #sock.sendall(result[1])
+    print("Results sent! Closing socket")
+    sock.shutdown(socket.SHUT_WR)
     sock.close()
 
-def start_server(host='127.0.0.1', port=57394):
+
+def start_server(host="127.0.0.1", port=57394):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    
+
     server_socket.bind((host, port))
-    
+
     server_socket.listen(5)
     print(f"Server started on {host}:{port}")
 
@@ -24,20 +30,26 @@ def start_server(host='127.0.0.1', port=57394):
         while True:
             client_socket, client_address = server_socket.accept()
             print(f"{client_address} connected.")
-            
-            #reading an str for client_handler(arg)
-            data = ''
-            part=client_socket.recv(1024).decode('utf-8')
-            print('Recieving package')
+
+            # reading an str for client_handler(arg)
+            data = ""
+            part = client_socket.recv(1024).decode("utf-8")
+            print("Recieving package")
             while part:
-                data+= part
-                part = client_socket.recv(1024).decode('utf-8')
+                data += part
+                part = client_socket.recv(1024).decode("utf-8")
                 print(data)
-            print(f'Packet recieved: {data}')
-            
-            process = Process(target=client_handler_server, args=(data,client_socket,))
+            print(f"Packet recieved: {data}")
+
+            process = Process(
+                target=client_handler_server,
+                args=(
+                    data,
+                    client_socket,
+                ),
+            )
             process.start()
-            #client_socket.close()
+            # client_socket.close()
     except KeyboardInterrupt:
         print("Server paused")
     finally:
