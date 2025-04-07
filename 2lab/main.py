87897734fffec6f1ac82da6f6e66bc7baee7e1db
kvdep <jack_ggg@mail.ru>
2025-04-07 19:50:07 +0300

    added uvicorn run to main

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.visualise import router as vis_router
 from app.api.graph import router as graph_router
+import uvicorn
 
 app = FastAPI()
 #app.include_router(scrape_router, prefix="/scrape", tags=["Scraping"])
@@ -8,6 +9,7 @@ app.include_router(graph_router, prefix="/graph", tags=["Graph"])
 app.include_router(vis_router, prefix="/vis", tags=["Visualise"])
 
 def main():
+    uvicorn.run(app, host="0.0.0.0", port=8001)
     pass
 
 if __name__ == "__main__":
